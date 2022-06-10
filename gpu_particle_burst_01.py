1d503e067614380bff0ebd2a2c2855061433da74
Bruno Cabral <cabral.brc@gmail.com>
2022-06-09 20:59:04 -0700

    added OpenGL Shaders

--- gpu_particle_burst_01.py
+++ gpu_particle_burst_01.py
@@ -1,11 +1,22 @@
 """
 Example showing how to create particle explosions via the GPU.
 """
+from array import array
+from dataclasses import dataclass
 import arcade
+import arcade.gl
 
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
 SCREEN_TITLE = "GPU Particle Explosion"
+
+
+@dataclass
+class Burst:
+    """Track for each burst."""
+
+    buffer: arcade.gl.Buffer
+    vao: arcade.gl.Geometry
 
 
 class MyWindow(arcade.Window):
@@ -13,6 +24,15 @@ class MyWindow(arcade.Window):
 
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.burst_list = []
+
+        # Program to visualize the points
+        self.program = self.ctx.load_program(
+            vertex_shader="vertex_shader_v1.glsl",
+            fragment_shader="fragment_shader.glsl",
+        )
+
+        self.ctx.enable_only()
 
     def on_draw(self):
         """Draw everything"""
