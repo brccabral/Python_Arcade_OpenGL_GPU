4be5d6ef0e7d58a49547d948fa4950357f1aa77d
Bruno Cabral <cabral.brc@gmail.com>
2022-06-09 21:01:21 -0700

    mouse click adds one point on screen

--- gpu_particle_burst_01.py
+++ gpu_particle_burst_01.py
@@ -38,13 +38,46 @@ class MyWindow(arcade.Window):
         """Draw everything"""
         self.clear()
 
+        # Set the particle size
+        self.ctx.point_size = 2 * self.get_pixel_ratio()
+
+        # Loop through each burst
+        for burst in self.burst_list:
+
+            # Render the burst
+            burst.vao.render(self.program, mode=self.ctx.POINTS)
+
     def on_update(self, dt):
         """Update everything"""
         pass
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
         """User clicks mouse"""
-        pass
+
+        def _gen_initial_data(initial_x, initial_y):
+            """Generate data for each particle"""
+            yield initial_x
+            yield initial_y
+
+        # Recalculate the coordinates from pixels to the OpenGL system with
+        # 0, 0 at the center.
+        x2 = x / self.width * 2.0 - 1.0
+        y2 = y / self.height * 2.0 - 1.0
+
+        # Get initial particle data
+        initial_data = _gen_initial_data(x2, y2)
+
+        # Create a buffer with that data
+        buffer = self.ctx.buffer(data=array("f", initial_data))
+
+        # Create a buffer description that says how the buffer data is formatted.
+        buffer_description = arcade.gl.BufferDescription(buffer, "2f", ["in_pos"])
+        # Create our Vertex Attribute Object
+        vao = self.ctx.geometry([buffer_description])
+
+        # Create the Burst object and add it to the list of bursts
+        burst = Burst(buffer=buffer, vao=vao)
+        self.burst_list.append(burst)
 
 
 if __name__ == "__main__":
