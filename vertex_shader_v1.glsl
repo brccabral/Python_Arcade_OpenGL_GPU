1a182d062aed965bbdbda1b79e0aee6f06511431
Bruno Cabral <cabral.brc@gmail.com>
2022-06-09 21:26:46 -0700

    Add Gravity

--- vertex_shader_v1.glsl
+++ vertex_shader_v1.glsl
@@ -27,8 +27,12 @@ void main() {
     // Set the RGBA color
     color = vec4(in_color[0], in_color[1], in_color[2], alpha);
 
+    // Adjust velocity based on gravity
+    vec2 new_vel = in_vel;
+    new_vel[1] -= time * 0.7;
+
     // Calculate a new position
-    vec2 new_pos = in_pos + (time * in_vel);
+    vec2 new_pos = in_pos + (time * new_vel);
 
     // Set the position. (x, y, z, w)
     gl_Position = vec4(new_pos, 0.0, 1);
